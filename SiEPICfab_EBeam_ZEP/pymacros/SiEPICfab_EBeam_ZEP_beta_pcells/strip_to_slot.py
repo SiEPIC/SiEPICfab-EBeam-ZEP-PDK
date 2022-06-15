f93288cc9ccd5b6081797779f9e49669d58b1170
Lukas Chrostowski <lukasc@ece.ubc.ca>
2022-06-14 23:27:23 -0700

    Update strip_to_slot.py
    - improved design, 99% efficiency simulated
    - use make_pin function @jaspreetj @mustafacc @atofini

--- SiEPICfab_EBeam_ZEP/pymacros/SiEPICfab_EBeam_ZEP_beta_pcells/strip_to_slot.py
+++ SiEPICfab_EBeam_ZEP/pymacros/SiEPICfab_EBeam_ZEP_beta_pcells/strip_to_slot.py
@@ -4,10 +4,15 @@ from pya import *
 class strip_to_slot(pya.PCellDeclarationHelper):
     """
     The PCell declaration for the strip_to_slot.
+    based on https://www.osapublishing.org/oe/fulltext.cfm?uri=oe-21-16-19029&id=259920
+
     July 24, 2017 - Lukas Chrostowski
     March 17, 2021- Alexander Tofini
     June 13, 2022 - Jaspreet Jhoja
-    based on https://www.osapublishing.org/oe/fulltext.cfm?uri=oe-21-16-19029&id=259920
+    June 14, 2022 - Lukas Chrostowski
+      - reduce the curved waveguide section, increase straight section for lower loss
+      - simulated using EME to give 99% efficiency for TE 1550 nm.
+
     """
 
     def __init__(self):       
@@ -17,18 +22,21 @@ class strip_to_slot(pya.PCellDeclarationHelper):
         # declare the parameters
         self.param("silayer", self.TypeLayer, "Si Layer", default = TECHNOLOGY['Si_core'])
         self.param("clad", self.TypeLayer, "Cladding Layer", default = TECHNOLOGY['Si_clad'])
-        self.param("r", self.TypeDouble, "Radius", default = 10)
+        self.param("r", self.TypeDouble, "Radius", default = 15)
         self.param("w", self.TypeDouble, "Waveguide Width", default = 0.5)
         self.param("rails", self.TypeDouble, "Rails", default=0.25) 
         self.param("slot", self.TypeDouble, "Slot", default=0.1)
-        self.param("taper",self.TypeDouble,"Taper Length",default=10.0)   
+        self.param("taper",self.TypeDouble,"Taper Length",default=15.0)   
+        self.param("wt", self.TypeDouble, "Terminated Curved Width", default = 0.1)
+        self.param("w_cladding", self.TypeDouble, "Cladding, from center", default = 2.25)
+        self.param("offset", self.TypeDouble, "Ending Offset for Curved section", default = 1.0)
         self.param("pinrec", self.TypeLayer, "PinRec Layer", default = TECHNOLOGY['PinRec'])
         self.param("devrec", self.TypeLayer, "DevRec Layer", default = TECHNOLOGY['DevRec'])
         self.param("textl", self.TypeLayer, "Text Layer", default = TECHNOLOGY['Text'])
     
     def display_text_impl(self):
         # Provide a descriptive text for the cell
-        return "Strip_To_Slot(rails=" + ('%.3f' % self.rails) + ",slot=" + ('%g' % ( self.slot)) + ")"
+        return "strip_to_slot(rails=" + ('%.3f' % self.rails) + ",slot=" + ('%g' % ( self.slot)) + ")"
 
     def can_create_from_shape_impl(self):
         return False
@@ -36,11 +44,12 @@ class strip_to_slot(pya.PCellDeclarationHelper):
     def produce_impl(self):
         # This is the main part of the implementation: create the layout
      
-        from math import pi, cos, sin
+        from math import pi, cos, sin, acos, sqrt
         from SiEPIC.utils import arc
         from SiEPIC.utils import arc_xy
         from SiEPIC.extend import to_itype
         from SiEPIC.utils import arc_wg_xy
+        from SiEPIC.utils.layout import make_pin
         
         # fetch the parameters
         dbu = self.layout.dbu
@@ -60,20 +69,18 @@ class strip_to_slot(pya.PCellDeclarationHelper):
         slot = to_itype(self.slot, dbu)
         rails = to_itype(self.rails, dbu)
         taper = to_itype(self.taper,dbu)
+        wt = to_itype(self.wt, dbu)
+        offset = to_itype(self.offset, dbu)
+        w_cladding = to_itype(self.w_cladding, dbu)
+
         # draw the quarter-circle
         x = 0
         y = r + rails/2 + w + slot
         
-        dilation = 2/dbu
-
         t = Trans(Trans.R0, x, y)
-        #self.cell.shapes(LayerSiN).insert(Path(arc(r, 270, 360), rails).transformed(t).simple_polygon())
-        arc_circle = arc_wg_xy(x,y,r,rails,270,360)
-        shapes(LayerSiN).insert(arc_circle)
-        
-        arc_circle = arc_wg_xy(x,y,r,rails,270,360)
-        arc_circle.size(dilation)
-        #shapes(LayerCladN).insert(arc_circle)
+        theta = acos((r-offset)/r)/2/pi*360
+        arc_width = sqrt( r**2 - (r-offset)**2)
+        self.cell.shapes(LayerSiN).insert(arc_wg_xy(x,y,r+rails/2-wt/2,wt,270,270+theta))
             
         pts = []     
         pts.append(Point.from_dpoint(DPoint(0, w)))
@@ -83,55 +90,29 @@ class strip_to_slot(pya.PCellDeclarationHelper):
         polygon = Polygon(pts)
         shapes(LayerSiN).insert(polygon)
         
-        polygon = Polygon(pts)
-        polygon.size(dilation)
-        #shapes(LayerCladN).insert(polygon)
-        clad_shape = pya.Region(arc_circle) + pya.Region(polygon)
-
-        
         # Create the top left 1/2 waveguide
-        wg1 = Box(-taper, w+slot, 0, w+rails+slot)
+        wg1 = Polygon([
+                Point.from_dpoint(DPoint(-taper, w+slot)),Point.from_dpoint(DPoint(-taper, w+slot+rails)),
+                Point.from_dpoint(DPoint(0, w+slot+wt)),Point.from_dpoint(DPoint(0, w+slot))])
         shapes(LayerSiN).insert(wg1)
         
-        wg1 = Box(-taper, w+slot, 0, w+rails+slot)
-        wg1_l = wg1.enlarged(dilation, dilation)
-        clad_shape = clad_shape + pya.Region(wg1_l)
-        #shapes(LayerCladN).insert(wg1_l)
-
-        from math import pi, cos, sin
-        from SiEPIC.utils import arc_wg, arc_wg_xy
-        from SiEPIC._globals import PIN_LENGTH as pin_length
-        
-
         # Create the waveguide
-        wg1 = Box(0, 0, r+w+dilation , w )
+        wg1 = Box(0, 0, arc_width+wt , w )
         shapes(LayerSiN).insert(wg1)
-        
-        wg1 = Box(0, 0, r+w+dilation , w )
-        wg1_l = wg1.enlarged(0, dilation)
-        #shapes(LayerCladN).insert(wg1_l)
-        clad_shape = clad_shape + pya.Region(wg1_l)
-        
+
         # Pin on the slot waveguide side:
-        shapes(LayerPinRecN).insert(pya.Path([pya.Point(-taper+0.05/dbu, w+slot/2),
-                                              pya.Point(-taper-0.05/dbu , w+slot/2)],2*rails+slot))
-        shapes(LayerPinRecN).insert(pya.Text("opt1", pya.Trans(pya.Trans.R0, -taper, w))).text_size = 0.5 / dbu
+        make_pin(self.cell, "opt1", [-taper,w+slot/2], 2*rails+slot,  LayerPinRecN, 180)
+        # Pin on the strip waveguide side:
+        make_pin(self.cell, "opt2", [int(arc_width+wt),w/2], w,  LayerPinRecN, 0)
         
-        # Pin on the bus waveguide side:
-        shapes(LayerPinRecN).insert(pya.Path([pya.Point(r+w+dilation-0.05/dbu, w/2),
-                                              pya.Point(r+w+dilation+0.05/dbu , w/2)],w))
-        shapes(LayerPinRecN).insert(pya.Text("opt2", pya.Trans(pya.Trans.R0, r+w+dilation, w/2))).text_size = 0.5 / dbu
-        
-        # Create the device recognition layer -- make it 1 * wg_width away from the waveguides.
-        dev = Box(-taper, -w / 2 - w - dilation, r  + w+dilation , y+dilation)
-        devrec_region = pya.Region(dev)
-        
-        clad_shape.merge()
-        
-        clad_shape = devrec_region & clad_shape
-        
-        shapes(LayerCladN).insert(clad_shape)
-        
-        shapes(LayerDevRecN).insert(dev)
+        # Create the device recognition and cladding layers
+        pts = []     
+        pts.append(Point.from_dpoint(DPoint(-taper, w+slot/2-w_cladding)))
+        pts.append(Point.from_dpoint(DPoint(-taper, w+slot/2+w_cladding)))
+        pts.append(Point.from_dpoint(DPoint(arc_width+wt, w/2+w_cladding )))
+        pts.append(Point.from_dpoint(DPoint(arc_width+wt, w/2-w_cladding )))
+        polygon = Polygon(pts)
+        shapes(LayerCladN).insert(polygon)
+        shapes(LayerDevRecN).insert(polygon)
 
         
